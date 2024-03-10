9310c7531fd7d93bd4c51a593fa94daecd8ca466
Mr-Destructive <gormeet711@gmail.com>
2024-03-10 13:33:35 +0530

    chore: update get for response parsing json

---
+++ web/methods/get/unmarshal.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+/*
+GET
+https://dummyjson.com/products/1
+
+{"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://cdn.dummyjson.com/product-images/1/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/1/1.jpg","https://cdn.dummyjson.com/product-images/1/2.jpg","https://cdn.dummyjson.com/product-images/1/3.jpg","https://cdn.dummyjson.com/product-images/1/4.jpg","https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"]}
+*/
+
+// we define a struct for the json data
+// we can only include the fields that we want
+type Product struct {
+	ID                 int     `json:"id"`
+	Title              string  `json:"title"`
+	Description        string  `json:"description"`
+	Price              float64 `json:"price"`
+	DiscountPercentage float64 `json:"discountPercentage"`
+	Rating             float64 `json:"rating"`
+	Stock              int     `json:"stock"`
+	Brand              string  `json:"brand"`
+	Category           string  `json:"category"`
+	/*
+	   // we can skip the fields that we don't want
+	      Thumbnail          string  `json:"thumbnail"`
+	      Images             []string `json:"images"`
+	   // or include omitempty flag
+	   // the omitempty flag will skip the field if it is empty
+	       Thumbnail          string  `json:"thumbnail,omitempty"`
+	       Images             []string `json:"images,omitempty"`
+	*/
+}
+
+func main() {
+	reqURL := "https://dummyjson.com/products/1"
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var data Product
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+	//we can now access the data with the struct
+	fmt.Println(data.Title)
+}
